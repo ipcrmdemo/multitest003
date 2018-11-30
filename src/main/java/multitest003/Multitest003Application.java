0239e1266c9a201eed56621c5fed0ed3be90f722
Matt Cadorette <mcadorette@gmail.com>
2018-11-30 15:17:09 -0500

    Initial commit from Atomist

---
+++ src/main/java/multitest003/Multitest003Application.java
@@ -0,0 +1,12 @@
+package multitest003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Multitest003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Multitest003Application.class, args);
+	}
+}
